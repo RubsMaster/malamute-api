cdf565f28385f989a203163972e3aca291434529
rubsmaster <jrubsb@gmail.com>
2025-01-30 17:45:50 -0600

    commented db connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import db from "./config/db.js";
+// import db from "./config/db.js";
 
 app.listen(process.env.PORT, ()=> {
     console.log(`listening on port ${process.env.PORT}`);

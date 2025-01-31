bd4a8ba18b23fae2acd2761cb495c4a649f53d5e
rubsmaster <jrubsb@gmail.com>
2025-01-30 18:01:34 -0600

    aaaa

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -1,15 +1,16 @@
 // import db from "../config/db.js";
 export const getAllExercises = async (req, res) => {
-    try {
-        const query = `
-            SELECT *
-            FROM exercises
-          `;
-        const result = await db.query(query);
-        console.log("consultado")    
-        res.status(200).json(result.rows);
-      } catch (error) {
-        console.error("Error fetching restaurants:", error);
-        res.status(500).send("Internal Server Error");
-      }
-}
+  res.send("hola");
+//     try {
+//         const query = `
+//             SELECT *
+//             FROM exercises
+//           `;
+//         const result = await db.query(query);
+//         console.log("consultado")    
+//         res.status(200).json(result.rows);
+//       } catch (error) {
+//         console.error("Error fetching restaurants:", error);
+//         res.status(500).send("Internal Server Error");
+//       }
+ }

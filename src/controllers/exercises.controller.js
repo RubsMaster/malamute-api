c5318e2f1619a991342b7ea2f50c2669dca9b0ea
rubsmaster <jrubsb@gmail.com>
2025-02-01 09:51:01 -0600

    se activo la bd

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -1,16 +1,5 @@
-// import db from "../config/db.js";
+import db from "../config/db.js";
 export const getAllExercises = async (req, res) => {
-  res.send("hola");
-//     try {
-//         const query = `
-//             SELECT *
-//             FROM exercises
-//           `;
-//         const result = await db.query(query);
-//         console.log("consultado")    
-//         res.status(200).json(result.rows);
-//       } catch (error) {
-//         console.error("Error fetching restaurants:", error);
-//         res.status(500).send("Internal Server Error");
-//       }
+  console.log("si sucede un error se viene pa aca")
+  res.send("cliente");
  }

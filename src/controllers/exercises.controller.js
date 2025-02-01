1a9a3d0bc0de07a966717602dd5b0c2027ef6018
rubsmaster <jrubsb@gmail.com>
2025-02-01 11:51:02 -0600

    se puso el fetch en la ruta

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -1,5 +1,15 @@
 import db from "../config/db.js";
 export const getAllExercises = async (req, res) => {
-  console.log("si sucede un error se viene pa aca")
-  res.send("cliente");
+  try {
+    const query = `
+        SELECT *
+        FROM exercises
+      `;
+    const result = await db.query(query);
+    console.log("consultado")    
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error fetching restaurants:", error);
+    res.status(500).send("Internal Server Error");
+  }
  }

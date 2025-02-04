0464cf6004132e0bcfbacf793eaa8688428ec0b3
rubsmaster <jrubsb@gmail.com>
2025-02-04 14:00:10 -0600

    pagination and join to get names, changing the type of the data

--- src/controllers/exercises.controller.js
+++ src/controllers/exercises.controller.js
@@ -1,15 +1,48 @@
 import db from "../config/db.js";
 export const getAllExercises = async (req, res) => {
   try {
-    const query = `
-        SELECT *
-        FROM exercises
-      `;
-    const result = await db.query(query);
-    console.log("consultado")    
-    res.status(200).json(result.rows);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const search = req.query.search || "";
+    const offset = (page - 1) * limit;
+
+    const dataQuery = `
+  SELECT 
+    e.*,
+    bp.name AS bodypart_name,
+    eq.name AS equipment_name,
+    wt.name AS type_name
+  FROM exercises e
+  LEFT JOIN musculargroup bp ON e.bodypart::INTEGER = bp.id
+  LEFT JOIN equipment eq ON e.equipment::INTEGER = eq.id
+  LEFT JOIN workouttype wt ON e.type = wt.id
+  WHERE e.name ILIKE $3
+  ORDER BY e.id
+  LIMIT $1 OFFSET $2
+`;
+
+const countQuery = `
+SELECT COUNT(*) 
+FROM exercises e
+WHERE e.name ILIKE $1
+`;
+
+    const searchTerm = `%${search}%`;
+
+    const [dataResult, countResult] = await Promise.all([
+      db.query(dataQuery, [limit, offset, searchTerm]),
+      db.query(countQuery, [searchTerm]),
+    ]);
+
+    const total = parseInt(countResult.rows[0].count);
+    const totalPages = Math.ceil(total / limit);
+
+    res.status(200).json({
+      data: dataResult.rows,
+      pagination: { page, limit, total, totalPages },
+    });
   } catch (error) {
-    console.error("Error fetching restaurants:", error);
+    console.error("Error fetching exercises:", error);
     res.status(500).send("Internal Server Error");
   }
- }
+};
